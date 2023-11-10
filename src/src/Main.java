84bf9d844888a35216133b568adb8a4862d03d86
Davih Duque <davihduque8@gmail.com>
2023-11-10 02:59:21 -0300

    Adicionando funcionalidade de Salvar e Carregar, Fazer uma limpeza no código para ficar mais agradavel

--- src/src/Main.java
+++ src/src/Main.java
@@ -4,6 +4,7 @@ import Cliente.PessoaFisica;
 import Cliente.PessoaJuridica;
 
 // Importação das subclasses de Conta do pacote Conta
+import Conta.Conta;
 import Conta.ContaCorrente;
 import Conta.ContaPoupanca;
 import Conta.ContaUniversitaria;
@@ -22,32 +23,30 @@ public class Main {
 
         // Inicialização de dados
         PessoaFisica pF = new PessoaFisica("João", "Rua A", "12345678900", 30, 'M');
-        ContaPoupanca contaC = new ContaPoupanca(1, "123", 20000, "João", 1001, pF);
+        ContaPoupanca contaP = new ContaPoupanca(1, "123", 20000, "João", 1000, pF);
+
+        PessoaFisica duqueP = new PessoaFisica("Duque", "Rua A", "12345678901", 18, 'M');
+        ContaCorrente duque = new ContaCorrente(2, "12", 200, "Duque", 200, duqueP);
 
         List<Cliente> listaClientes = new ArrayList<>();
         listaClientes.add(pF);
+        listaClientes.add(duqueP);
 
         System.out.println("======Sistema Bancário======");
+
         System.out.println("Donos registrados:");
         for(Cliente c: listaClientes) {
             System.out.println(c.nome);
         }
-
-        // Tratando a Exception
-    try {
-        contaC.sacar(100);
-        contaC.sacar(200);
-        contaC.depositar(3000);
-    } catch(NegativoException | LimiteException erro) {
-        System.out.println(erro.getMessage());
-    }
-
-        // Impressão no Terminal
         System.out.println();
 
-        // Imprimindo Extrato de acordo com tipo inserido
-        contaC.imprimirExtrato(2);
-        System.out.println();
-        contaC.imprimirExtratoTaxas();
+        duque.salvarConta();
+
+        // Carregar a conta
+        Conta contaCarregada = Conta.carregarConta("2-Duque.ser");
+        if (contaCarregada != null) {
+            System.out.println("Conta carregada:\n" + contaCarregada);
+        }
     }
+
 }
